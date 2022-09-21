458c6998fd55ada5505f5cb1dd006b310ba15a43
Qingzhou Yan <qingzhouyan@gmail.com>
2022-09-20 22:50:35 -0700

    add public folder

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 var cors = require('cors')
+const path = require('path')
 
 const app = express()
 const router = require('./routes')
@@ -8,6 +9,7 @@ const router = require('./routes')
 const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/qa', router)
 
 app.listen(PORT, () => {
